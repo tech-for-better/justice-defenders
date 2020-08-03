0ba238fcd90bb64d0401b5eed8f1ce2278bb7e8e
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-08-03 16:33:01 +0100

    Reconfig electron set-up and add logo

--- src/electron.js
+++ src/electron.js
@@ -13,13 +13,17 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + "/../public/JD.png",
+  });
 
   // and load the index.html of the app.
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, "/../build/index.html"),
+      pathname: path.join(__dirname, "/../public/index.html"),
       protocol: "file:",
       slashes: true,
     });
