1d7a562926f04ba6d7f98d6b611eb62671abfca1
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-08-03 16:36:50 +0100

    Reconfig electron set-up and add logo

--- src/electron.js
+++ src/electron.js
@@ -23,7 +23,7 @@ function createWindow() {
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, "/../public/index.html"),
+      pathname: path.join(__dirname, "/../build/index.html"),
       protocol: "file:",
       slashes: true,
     });
