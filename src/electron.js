325660809e96b1760dfa8c0f7a3ff254a72214ee
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-08-18 10:31:28 +0100

    Fix routing from media cards

--- src/electron.js
+++ src/electron.js
@@ -16,7 +16,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: __dirname + "/assets/JD.png",
+    icon: __dirname + "/public/assets/JD.png",
   });
 
   // and load the index.html of the app.
