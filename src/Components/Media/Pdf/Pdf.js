e78d5380ffdc7e26a3f8d0400be621bf85f5bced
Giovanna Aveiro <to.gio@pm.me>
2020-08-19 14:14:39 +0100

    Change pdf component to open up in overlay window; reading page now shows a list of articles Relates #14

--- src/Components/Media/Pdf/Pdf.js
+++ src/Components/Media/Pdf/Pdf.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 const Pdf = ({ title, src }) => {
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: "absolute",
+      width: "80%",
+      height: "90%",
+      border: "2px solid black",
+      boxShadow: theme.shadows[5],
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <>
-    <h2>{title}</h2>
-    <embed src={src} width="500" height="375" type="application/pdf"></embed>
-    </>
+    <div className={classes.paper}>
+      <embed src={src} width="100%" height="100%"></embed>
+    </div>
   );
 };
 
