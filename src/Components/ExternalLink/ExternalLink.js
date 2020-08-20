951c679a99e3a4139ce5cf7d7d91d54f405c4cf6
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-08-20 10:58:58 +0100

    Add preload file to import required electron features for front end application Relates #49

--- src/Components/ExternalLink/ExternalLink.js
+++ src/Components/ExternalLink/ExternalLink.js
@@ -2,9 +2,21 @@ import React from "react";
 
 import { XternalLink } from "../Styles/Typography";
 
+const { shell } = window;
+
 const ExternalLink = ({ href, textContent }) => {
+  const defaultBrowser = (event) => {
+    event.preventDefault();
+    let link = event.target.href;
+    shell.openExternal(link);
+  };
+
   return (
-    <XternalLink data-cy='external-link' href={href} target="_blank">
+    <XternalLink
+      data-cy="external-link"
+      href={href}
+      onClick={defaultBrowser}
+      target="_blank">
       {textContent}
     </XternalLink>
   );
