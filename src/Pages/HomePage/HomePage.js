97917ec9d0860b6bdd4b48035740bbb32093ecf1
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-08-27 16:21:34 +0100

    Add intro video for home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import YouTubePlayer from "react-player/lib/players/YouTube";
 
 // components
 import Navbar from "../../Components/Navbars/Navbar";
@@ -7,6 +8,7 @@ import ExternalLink from "../../Components/ExternalLink/ExternalLink";
 // styles
 import { MainTitle } from "../../Components/Styles/Typography";
 import { PageWrapper } from "../../Components/Styles/Containers";
+import { YoutubeContainer } from "./HomePage.style";
 
 const HomePage = () => {
   return (
@@ -16,6 +18,16 @@ const HomePage = () => {
         <MainTitle>
           Welcome to the Justice Defenders LLB learning platform!
         </MainTitle>
+        <YoutubeContainer>
+          <iframe
+            title="intro-video"
+            width="860"
+            height="485"
+            src="https://www.youtube.com/embed/bOHtZ1l_1Q8"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </YoutubeContainer>
         <ExternalLink
           href={"https://www.justice-defenders.org/"}
           textContent={"Visit Justice Defenders website →"}
---
+++ src/Pages/HomePage/HomePage.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const YoutubeContainer = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5rem;
+`
+
+export { YoutubeContainer }
